23e28ed028e2a9f72891ae993ad11a4213e6bc32
Lan Kim Ho <lan.ho@asnet.com.vn>
2025-11-28 17:10:24 +0700

    feat(shadcn): add shadcn

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,13 @@
 import { sum } from "@repo/math/math.ts";
+import { Button } from "@repo/ui/components/button";
 
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div className="size-10 bg-blue-1000">Apps</div>
       <div className="size-10 bg-red-1000">Red-1000</div>
-      <div className="size-10 bg-primary-1000" />
+      <div className="size-10 bg-primary" />
+      <Button variant="default">123</Button>
       {sum(1, 2)}
     </main>
   );
