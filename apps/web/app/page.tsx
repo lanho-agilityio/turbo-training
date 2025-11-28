4b416f74ce8557d5ec7a15bd6a7036ac6f2ca669
Lan Kim Ho <lan.ho@asnet.com.vn>
2025-11-28 15:28:38 +0700

    feat(math): add package math

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,11 @@
+import { sum } from "@repo/math/math.ts";
+
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div className="size-10 bg-blue-1000">Apps</div>
       <div className="size-10 bg-red-1000">Red-1000</div>
+      {sum(1, 2)}
     </main>
   );
 }
