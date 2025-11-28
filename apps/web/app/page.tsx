f4fc66a9b60b9fec5efd88480e81cf3aef5094a9
Lan Kim Ho <lan.ho@asnet.com.vn>
2025-11-28 10:54:04 +0700

    update(ui): update packages ui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <div className="size-10 bg-blue-1000">1232</div>
+      <div className="size-10 bg-blue-1000">Apps</div>
+      <div className="size-10 bg-red-1000">Red-1000</div>
     </main>
   );
 }
