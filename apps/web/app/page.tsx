9794424db4faa1f1e6a53e580ccf0e8eb1a19a4f
Lan Kim Ho <lan.ho@asnet.com.vn>
2025-12-01 16:19:44 +0700

    feat(effect): try effect

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,26 @@
 "use client";
-import { useState, useMemo, useCallback } from "react";
+import { useState, useMemo, useCallback, useEffect } from "react";
 import { sum } from "@repo/math/math.ts";
 import { Button } from "@repo/ui/components/button";
-import { Effect } from "effect";
+import { Effect, Exit } from "effect";
+
+const log = (message: string) => Effect.sync(() => console.log(message));
+
+const parse = (input: string) =>
+  // This might throw an error if input is not valid JSON
+  Effect.try(() => JSON.parse(input));
+
+const delay = (message: string) =>
+  Effect.promise<string>(
+    () =>
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(message);
+        }, 2000);
+      })
+  );
+
+Effect.log("Hello world!");
 
 export default function Page() {
   const [count, setCount] = useState(0);
@@ -13,6 +31,36 @@ export default function Page() {
   );
 
   const increment = useCallback(() => Effect.runSync(task), [task]);
+
+  useEffect(() => {
+    Effect.runSync(log("hello from React"));
+  }, []);
+
+  useEffect(() => {
+    Effect.runPromise(delay("123312323"))
+      .then((res) => console.log(res))
+      .catch((err) => console.error("Error:", err));
+  }, []);
+
+  // console.log(Effect.runSyncExit(Effect.succeed(1)));
+  // console.log(Effect.runSyncExit(Effect.fail("my error")));
+  // console.log(Effect.runSyncExit(Effect.promise(() => Promise.resolve(1))));
+
+  const [state, setState] = useState<
+    | { status: "loading" }
+    | { status: "success"; value: unknown }
+    | { status: "error"; cause: unknown }
+  >({ status: "loading" });
+
+  useEffect(() => {
+    Effect.runPromiseExit(parse("bad json")).then((exit) => {
+      if (Exit.isSuccess(exit)) {
+        setState({ status: "success", value: exit.value });
+      } else {
+        setState({ status: "error", cause: exit.cause });
+      }
+    });
+  }, []);
 
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
